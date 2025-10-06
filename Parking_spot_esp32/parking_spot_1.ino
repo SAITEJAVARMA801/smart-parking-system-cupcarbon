c295fff07c28c94225f52ae7d9235afd1ef7c8ed
SAI TEJA VARMA <109424031+SAITEJAVARMA801@users.noreply.github.com>
2025-10-06 13:40:59 +0530

    Update parking_spot_1.ino

--- Parking_spot_esp32/parking_spot_1.ino
+++ Parking_spot_esp32/parking_spot_1.ino
@@ -3,14 +3,14 @@
 #include <Adafruit_NeoPixel.h>
 
 // ====== USER CONFIG ======
-#define WIFI_SSID     "OPPO_RENO_8"
-#define WIFI_PASS     "STV@9996"
+#define WIFI_SSID     "YOURWIFI"
+#define WIFI_PASS     "PASSWORD"
 
 //  spot ID
 #define SPOT_ID 1
 
-// MQTT broker (central computer IP)
-#define MQTT_HOST     "10.217.88.250"
+// MQTT broker
+#define MQTT_HOST     "computer ip"
 #define MQTT_PORT     1883
 
 // Pins
